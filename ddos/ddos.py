7d48df883888d1a35b2ac6d64f9e2a2a7cb65a6e
Руслан <@DESKTOP-AIH9B8O>
2023-09-25 19:01:11 +0300

    Добавьте файлы проекта.

---
+++ ddos/ddos.py
@@ -0,0 +1,21 @@
+import telnetlib
+
+#host = #4001
+
+f = open('C:\\bebra.txt','r')
+
+com =  f.readlines()
+
+n = len(com)
+
+print(n)
+
+
+tell = telnetlib.Telnet('172.17.9.151',4001)
+print(com[10])
+i=0
+for i in range(n):
+    #tell.write(com[i].encode('utf-8') + b"\n")
+    print(com[i])
+    tell.write((com[i].replace('\n','') + '\n').encode('utf-8'))
+    i+=1   
